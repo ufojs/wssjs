79ba277289e9bd06e188f987be041f10b38979d1
Pasquale Boemio <boemianrapsodi@gmail.com>
2014-04-12 15:49:39 +0200

    if patch

--- patch.sh
+++ patch.sh
@@ -5,6 +5,8 @@ cp lib/wss.bundle.js lib/wss.bundle.js.old
 LN=`grep -n "if (Buffer._useTypedArrays && typeof subject.byteLength === 'number') {" lib/wss.bundle.js | awk 'BEGIN { FS = ":" } ; { print $1 }'`
 ALL=`wc -l lib/wss.bundle.js.old | awk '{ print $1 }'`
 
-head -n $(( $LN - 1 )) lib/wss.bundle.js.old > lib/wss.bundle.js
-echo "if (Buffer._useTypedArrays && typeof subject.byteLength === 'number') {" >> lib/wss.bundle.js
-tail -n $(( $ALL - $LN )) lib/wss.bundle.js.old >> lib/wss.bundle.js
+if [ "$LN" -ne "" ]; then
+  head -n $(( $LN - 1 )) lib/wss.bundle.js.old > lib/wss.bundle.js
+  echo "if (Buffer._useTypedArrays && typeof subject.byteLength === 'number') {" >> lib/wss.bundle.js
+  tail -n $(( $ALL - $LN )) lib/wss.bundle.js.old >> lib/wss.bundle.js
+fi
